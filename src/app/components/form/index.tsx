f1da5e10ab4c5149fae49b9872e988ff9915dab9
chelsofia <rachelasse01@gmail.com>
2025-02-14 22:58:23 +0100

    update

--- src/app/components/form/index.tsx
+++
@@ -1,5 +0,0 @@
-export {Input} from "@/app/components/form/input"
-// export {Button} from "@/app/components/button"
-export {Select} from "@/app/components/form/select"
-// export {FileUpload} from "@/app/components/fileupload/FileUpload"
-
